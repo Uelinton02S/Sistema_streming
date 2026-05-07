4d999c4eadaa8b79fd8d647e047d6d3453391549
Uelinton <uelintonsantiago13@gmail.com>
2026-05-07 20:32:14 -0300

    Create Filme.java
    subclasse

---
+++ Filme.java
@@ -0,0 +1,17 @@
+public class Filme extends Conteudo {
+
+    private String genero;
+
+    public Filme(String titulo, int duracao, int classificacaoIndicativa, String genero) {
+        super(titulo, duracao, classificacaoIndicativa);
+        this.genero = genero;
+    }
+
+    @Override
+    public void exibirDetalhes() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Duração: " + duracao + " minutos");
+        System.out.println("Classificação: " + classificacaoIndicativa + " anos");
+        System.out.println("Gênero: " + genero);
+    }
+}
